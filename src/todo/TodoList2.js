5207a9a4e0d620512de370a4cc2121810b0b829c
ddiz <ruroo@naver.com>
2020-09-14 19:05:02 +0900

    aws연결

--- src/todo/TodoList2.js
+++ src/todo/TodoList2.js
@@ -1,80 +1,119 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React,{ Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import AddTodo from "./AddTodo";
+import axios from 'axios'; 
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
+class TodoList2 extends Component {
 
-export default function TodoList2() {
-  const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
-  const [max_content_id, setMaxContentId] = React.useState(3);
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked : [],
+      max_content_id : 0,
+      contents: [],
+    };
+  }
+  //const [checked, setChecked] = React.useState([0]);
+  // const [max_content_id, setMaxContentId] = React.useState(3);
   // const [contents,setContents] = React.useState( [
   //   { id: 0, done: true, task: "공부하기" },
   //   { id: 1, done: false, task: "놀기" },
   //   { id: 2, done: false, task: "밥먹기" },
   // ]);
 
-  const [contents,setContents] = React.useState([]);
+  // const [contents,setContents] = React.useState([]);
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+  
+  loadingData = async () => { 
+    try { const response = await axios.get( 
+     //  'https://jsonplaceholder.typicode.com/todos/1' 
+      'http://13.124.83.214:5000/api/todo' 
+      ); 
+      this.setState({ 
+        // boards: 'test' 
+        contents: response.data, 
+       }); 
+       console.log(this.state.contents[0]._id);
+     } catch (e) { 
+       console.log(e);
+      } }; 
+      
+   //마운트 될때 실행 
+   componentDidMount() { 
+     const { loadingData } = this; 
+     loadingData(); 
+   }  
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
+  render() {
 
-    setChecked(newChecked);
-  };
+    // const useStyles = makeStyles((theme) => ({
+    //   root: {
+    //     width: '100%',
+    //     maxWidth: 360,
+    //     backgroundColor: theme.palette.background.paper,
+    //   },
+    // }));
+  
+    // const classes = useStyles();
 
-  return (
-    <div>
-    <List className={classes.root}>
-      {contents.map((value,i) => {
-        const labelId = `checkbox-list-label-${i}`;
+    const handleToggle = (value) => () => {
+      const currentIndex = this.state.checked.indexOf(value);
+      const newChecked = this.state.checked;
+  
+      if (currentIndex === -1) {
+        newChecked.push(value);
+      } else {
+        newChecked.splice(currentIndex, 1);
+      }
+  
+      this.setState({checked : newChecked});
+    };
 
-        console.log(value.task);
-
-
-        return (
-          <ListItem key={i} role={undefined} dense button onClick={handleToggle(i)}>
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={checked.indexOf(i) !== -1}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={value.task} />
-          </ListItem>
-        );
-      })}
-    </List>
-    <AddTodo onSubmit={function(_task){
-          const maxId = max_content_id + 1
-          setMaxContentId(maxId);
-          var _contents = contents.concat(
-            {id:max_content_id, done: false, task:_task}
-          );
-          console.log(max_content_id);
-          console.log(_contents);
-          setContents(_contents);
-        }}></AddTodo>
-    </div>
-  );
+    return (
+      <div>
+        {/* <List className={classes.root}> */}
+        <List>
+          {this.state.contents.map((value,i) => {
+            const labelId = `checkbox-list-label-${i}`;
+    
+            console.log(value.task);
+    
+    
+            return (
+              <ListItem key={i} role={undefined} dense button onClick={handleToggle(i)}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={this.state.checked.indexOf(i) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={value.task} />
+              </ListItem>
+            );
+          })}
+        </List>
+        <AddTodo onSubmit={function(_task){
+              const maxId = this.state.max_content_id + 1
+              this.setState( {max_content_id: maxId} );
+              var _contents = this.state.contents.concat(
+                {id:maxId, done: false, task:_task}
+              );
+              console.log(maxId);
+              console.log(_contents);
+              this.setState({contents:_contents});
+            }.bind(this)}>
+        </AddTodo>
+      </div>
+    );
+  }
 }
+
+export default TodoList2;
